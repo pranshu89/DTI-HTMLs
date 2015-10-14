5a9ef32ae85547824963592ac9c5887e067c96d2
pranshugupta <pranshu.anim.2007@gmail.com>
2015-10-14 23:33:27 +0530

    HTML Pages

---
+++ js/custom.js
@@ -0,0 +1,262 @@
+jQuery(document).ready(function() {
+   
+   // Toggle Left Menu
+   jQuery('.nav-parent > a').click(function() {
+      
+      var parent = jQuery(this).parent();
+      var sub = parent.find('> ul');
+      
+      // Dropdown works only when leftpanel is not collapsed
+      if(!jQuery('body').hasClass('leftpanel-collapsed')) {
+         if(sub.is(':visible')) {
+            sub.slideUp(200, function(){
+               parent.removeClass('nav-active');
+               jQuery('.mainpanel').css({height: ''});
+               adjustmainpanelheight();
+            });
+         } else {
+            closeVisibleSubMenu();
+            parent.addClass('nav-active');
+            sub.slideDown(200, function(){
+               adjustmainpanelheight();
+            });
+         }
+      }
+      return false;
+   });
+   
+   function closeVisibleSubMenu() {
+      jQuery('.nav-parent').each(function() {
+         var t = jQuery(this);
+         if(t.hasClass('nav-active')) {
+            t.find('> ul').slideUp(200, function(){
+               t.removeClass('nav-active');
+            });
+         }
+      });
+   }
+   
+   function adjustmainpanelheight() {
+      // Adjust mainpanel height
+      var docHeight = jQuery(document).height();
+      if(docHeight > jQuery('.mainpanel').height())
+         jQuery('.mainpanel').height(docHeight);
+   }
+   
+   
+   // Tooltip
+   jQuery('.tooltips').tooltip({ container: 'body'});
+   
+   // Popover
+   jQuery('.popovers').popover();
+   
+   // Close Button in Panels
+   jQuery('.panel .panel-close').click(function(){
+      jQuery(this).closest('.panel').fadeOut(200);
+      return false;
+   });
+   
+   
+		
+   // Form Toggles
+   jQuery('.toggle').toggles({on: true});
+   
+   jQuery('.toggle-chat1').toggles({on: false});
+   
+ 
+   
+   
+   // Minimize Button in Panels
+   jQuery('.minimize').click(function(){
+      var t = jQuery(this);
+      var p = t.closest('.panel');
+      if(!jQuery(this).hasClass('maximize')) {
+         p.find('.panel-body, .panel-footer').slideUp(200);
+         t.addClass('maximize');
+         t.html('&plus;');
+      } else {
+         p.find('.panel-body, .panel-footer').slideDown(200);
+         t.removeClass('maximize');
+         t.html('&minus;');
+      }
+      return false;
+   });
+   
+   
+   // Add class everytime a mouse pointer hover over it
+   jQuery('.nav-DemoCRM > li').hover(function(){
+      jQuery(this).addClass('nav-hover');
+   }, function(){
+      jQuery(this).removeClass('nav-hover');
+   });
+   
+   
+   // Menu Toggle
+   jQuery('.menutoggle').click(function(){
+      
+      var body = jQuery('body');
+      var bodypos = body.css('position');
+      
+      if(bodypos != 'relative') {
+         
+         if(!body.hasClass('leftpanel-collapsed')) {
+            body.addClass('leftpanel-collapsed');
+            jQuery('.nav-DemoCRM ul').attr('style','');
+            
+            jQuery(this).addClass('menu-collapsed');
+            
+         } else {
+            body.removeClass('leftpanel-collapsed chat-view');
+            jQuery('.nav-DemoCRM li.active ul').css({display: 'block'});
+            
+            jQuery(this).removeClass('menu-collapsed');
+            
+         }
+      } else {
+         
+         if(body.hasClass('leftpanel-show'))
+            body.removeClass('leftpanel-show');
+         else
+            body.addClass('leftpanel-show');
+         
+         adjustmainpanelheight();         
+      }
+
+   });
+   
+  
+   reposition_searchform();
+   
+   jQuery(window).resize(function(){
+      
+      if(jQuery('body').css('position') == 'relative') {
+
+         jQuery('body').removeClass('leftpanel-collapsed chat-view');
+         
+      } else {
+         
+         jQuery('body').removeClass('chat-relative-view');         
+         jQuery('body').css({left: '', marginRight: ''});
+      }
+      
+      reposition_searchform();
+      
+   });
+   
+   function reposition_searchform() {
+      if(jQuery('.searchform').css('position') == 'relative') {
+         jQuery('.searchform').insertBefore('.leftpanelinner .userlogged');
+      } else {
+         jQuery('.searchform').insertBefore('.header-right');
+      }
+   }
+
+});
+
+
+
+
+(function($) {
+    $.fn.tipsy = function(options) {
+
+        options = $.extend({}, $.fn.tipsy.defaults, options);
+        
+        return this.each(function() {
+            
+            var opts = $.fn.tipsy.elementOptions(this, options);
+            
+            $(this).hover(function() {
+
+                $.data(this, 'cancel.tipsy', true);
+
+                var tip = $.data(this, 'active.tipsy');
+                if (!tip) {
+                    tip = $('<div class="tipsy"><div class="tipsy-inner"/></div>');
+                    tip.css({position: 'absolute', zIndex: 100000});
+                    $.data(this, 'active.tipsy', tip);
+                }
+
+                if ($(this).attr('title') || typeof($(this).attr('original-title')) != 'string') {
+                    $(this).attr('original-title', $(this).attr('title') || '').removeAttr('title');
+                }
+
+                var title;
+                if (typeof opts.title == 'string') {
+                    title = $(this).attr(opts.title == 'title' ? 'original-title' : opts.title);
+                } else if (typeof opts.title == 'function') {
+                    title = opts.title.call(this);
+                }
+
+                tip.find('.tipsy-inner')[opts.html ? 'html' : 'text'](title || opts.fallback);
+
+                var pos = $.extend({}, $(this).offset(), {width: this.offsetWidth, height: this.offsetHeight});
+                tip.get(0).className = 'tipsy'; // reset classname in case of dynamic gravity
+                tip.remove().css({top: 0, left: 0, visibility: 'hidden', display: 'block'}).appendTo(document.body);
+                var actualWidth = tip[0].offsetWidth, actualHeight = tip[0].offsetHeight;
+                var gravity = (typeof opts.gravity == 'function') ? opts.gravity.call(this) : opts.gravity;
+
+                switch (gravity.charAt(0)) {
+                    case 'n':
+                        tip.css({top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}).addClass('tipsy-north');
+                        break;
+                    case 's':
+                        tip.css({top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}).addClass('tipsy-south');
+                        break;
+                    case 'e':
+                        tip.css({top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}).addClass('tipsy-east');
+                        break;
+                    case 'w':
+                        tip.css({top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}).addClass('tipsy-west');
+                        break;
+                }
+
+                if (opts.fade) {
+                    tip.css({opacity: 0, display: 'block', visibility: 'visible'}).animate({opacity: 0.8});
+                } else {
+                    tip.css({visibility: 'visible'});
+                }
+
+            }, function() {
+                $.data(this, 'cancel.tipsy', false);
+                var self = this;
+                setTimeout(function() {
+                    if ($.data(this, 'cancel.tipsy')) return;
+                    var tip = $.data(self, 'active.tipsy');
+                    if (opts.fade) {
+                        tip.stop().fadeOut(function() { $(this).remove(); });
+                    } else {
+                        tip.remove();
+                    }
+                }, 100);
+
+            });
+            
+        });
+        
+    };
+    
+    // Overwrite this method to provide options on a per-element basis.
+    // For example, you could store the gravity in a 'tipsy-gravity' attribute:
+    // return $.extend({}, options, {gravity: $(ele).attr('tipsy-gravity') || 'n' });
+    // (remember - do not modify 'options' in place!)
+    $.fn.tipsy.elementOptions = function(ele, options) {
+        return $.metadata ? $.extend({}, options, $(ele).metadata()) : options;
+    };
+    
+    $.fn.tipsy.defaults = {
+        fade: false,
+        fallback: '',
+        gravity: 'n',
+        html: false,
+        title: 'title'
+    };
+    
+    $.fn.tipsy.autoNS = function() {
+        return $(this).offset().top > ($(document).scrollTop() + $(window).height() / 2) ? 's' : 'n';
+    };
+    
+    $.fn.tipsy.autoWE = function() {
+        return $(this).offset().left > ($(document).scrollLeft() + $(window).width() / 2) ? 'e' : 'w';
+    };
+    
+})(jQuery);
